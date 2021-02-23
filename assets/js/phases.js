af1975bfd7c61ba3ff8a00732c2aec30de92418f
Felipe Guimarães <fguiengenharia@gmail.com>
2021-02-23 01:07:29 -0300

    Controle das áreas da tela

---
+++ assets/js/phases.js
@@ -0,0 +1,41 @@
+let phases = [
+    {
+        title: 'Vereador',
+        numbers: 5,
+        candidates: [
+            {
+                number: '38111',
+                name: 'Foda-se',
+                party: 'Foda-se 1',
+                photos: [
+                    { url: '38111.jpg', legend: 'Vereador' }
+                ],
+            }
+        ],
+    },
+
+    {
+        title: 'Prefeito',
+        numbers: 2,
+        candidates: [
+            {
+                number: '99',
+                name: 'Caguei',
+                party: 'Ninguém liga',
+                photos: [
+                    { url: '99.jpg', legend: 'Prefeito' },
+                    { url: '99_2.jpg', legend: 'Vice-Prefeito', small: true }
+                ],
+            },
+            {
+                number: '84',
+                name: 'Pau no cu',
+                party: 'Roubo mesmo',
+                photos: [
+                    { url: '84.jpg', legend: 'Prefeito' },
+                    { url: '84_2.jpg', legend: 'Vice-Prefeito', small: true }
+                ],
+            }
+        ],
+    }
+];
---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+let yourVoteFor = document.querySelector('.upside-1 span');
+
+let position = document.querySelector('.upside-2 span');
+
+let description = document.querySelector('.upside-4');
+
+let notification = document.querySelector('.bottom');
+
+let side = document.querySelector('.upside-right');
+
+let numbers = document.querySelector('.upside-3');
