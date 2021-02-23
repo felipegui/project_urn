0a88e5fb1a90066c372c57d6f7f980673912172a
Felipe Guimarães <fguiengenharia@gmail.com>
2021-02-23 17:12:08 -0300

    Preenchendo Interface com números

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Seleciona os items na tela da urna (Variáveis de controle de interface)
 let yourVoteFor = document.querySelector('.upside-1 span');
 
 let position = document.querySelector('.upside-2 span');
@@ -9,3 +10,73 @@ let notification = document.querySelector('.bottom');
 let side = document.querySelector('.upside-right');
 
 let numbers = document.querySelector('.upside-3');
+
+//Variáveis de controle de ambiente
+//Etapa atual
+let currentStep = 0;
+let number = '';
+
+//"Limpa" a tela, depois preenche a tela com as respectivas etapas atuais.
+function startStep() {
+    let phase = phases[currentStep];
+
+    let numberHtml = '';
+
+    for( let i=0; i<phase.numbers; i++ ) {
+        if( i==0 ) {
+            numberHtml = '<div class="number blink"></div>';
+        } else {
+            numberHtml += '<div class="number"></div>';
+        }    
+    }
+
+    yourVoteFor.style.display = 'none';
+
+    position.innerHTML = phase.title;
+
+    description.innerHTML = '';
+
+    notification.style.display = 'none';
+
+    side.innerHTML = '';
+
+    numbers.innerHTML = numberHtml;
+}
+
+//Função para atualizar a tela
+function updateInterface() {
+    alert("Finalizou de digitar o voto!");
+}
+
+//Ações nos botões
+function clicked(n) {
+    let numberBlink = document.querySelector('.number.blink');
+
+    if( numberBlink != null ) {
+        numberBlink.innerHTML = n;
+
+        number = `${number}${n}`;
+
+        numberBlink.classList.remove('blink');
+
+        if( numberBlink.nextElementSibling != null ) {
+            numberBlink.nextElementSibling.classList.add('blink');
+        } else {
+            updateInterface();
+        }
+    }
+}
+
+function white() {
+    alert("Clicou no BRANCO");
+}
+
+function corrects() {
+    alert("Clicou em CORRIGE");
+}
+
+function affirm() {
+    alert("Clicou em CONFIRMAR");
+}
+
+startStep();
